41e60ae3427e6d8b97f153dc20e98a12fa3b4fee
DevelSajjad <sinigdho01@gmail.com>
2022-12-31 10:30:55 +0600

    Add Category, Show Category and Delete Category

---
+++ admin/delete-category.php
@@ -0,0 +1,9 @@
+<?php
+
+    include("/BITM/wamp/www/News-Site/config.php");
+    $id = $_GET['delete'];
+    $sql = "delete from category where category_id = {$id}";
+    $result = mysqli_query($con, $sql);
+    header("location: http://localhost/News-Site/admin/category.php");
+
+?>
--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <span>© Copyright 2012 News </span>
+                <span>© Copyright 2022 News </span>
             </div>
         </div>
     </div>
